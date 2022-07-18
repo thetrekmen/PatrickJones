9f5e42a595cce64449953254722479cce1ba96e6
thetrekmen <jonespm1@gmail.com>
2022-07-18 12:23:13 -0400

    updated scrolling navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -648,15 +648,42 @@ loadWrapper.addEventListener('transitionend', () => {
 /* navbar scroll hide*/
 /* navbar scroll hide*/
 /* navbar scroll hide*/
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("header").style.top = "0";
-  } else {
-    document.getElementById("header").style.position = "fixed";
-    document.getElementById("header").style.top = "-100px";
-  }
-  prevScrollpos = currentScrollPos;
 
+const nav = document.getElementById("header");
+const navHeight = 200;
+// the point the scroll starts from (in px)
+let lastScrollY = 0;
+// how far to scroll (in px) before triggering
+const delta = 5;
+
+// function to run on scrolling
+function scrolled() {
+  let sy = window.scrollY;
+  // only trigger if scrolled more than delta
+  if (Math.abs(lastScrollY - sy) > delta) {
+    // scroll down -> hide nav bar
+    if (sy > lastScrollY && sy > navHeight) {
+      document.getElementById("header").style.position = "fixed";
+      document.getElementById("header").style.top = "-100px";
+    } 
+    // scroll up -> show nav bar
+    else if (sy < lastScrollY) {
+      document.getElementById("header").style.top = "0";
+    }
+   // update current scroll point
+   lastScrollY = sy 
+  }
 }
+
+// Add event listener & debounce so not constantly checking for scroll
+let didScroll = false;
+window.addEventListener("scroll", function(e){
+  didScroll = true;
+});
+
+setInterval(function() {
+  if (didScroll) {
+    scrolled();
+    didScroll = false;
+   }
+}, 100)
